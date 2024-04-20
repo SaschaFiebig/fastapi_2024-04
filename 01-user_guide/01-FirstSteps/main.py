7bfc47fb1c8ff7dbe90322c08f8eafb951946403
Sascha Fiebig <sascha.fiebig@posteo.net>
2024-04-20 16:11:42 +0200

    add comments for FirstSteps main.py

--- 01-user_guide/01-FirstSteps/main.py
+++ 01-user_guide/01-FirstSteps/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+# create fastapi instance 
+app = FastAPI()  # uvicorn main:app --reload (to start application)
+# my_awesome_api = FastAPI()  # uvicorn main:my_awesome_api --reload
 
+# CRUD functionalitity
+#@app.get()     Create
+#@app.post()    Read
+#@app.put()     Update
+#@app.delete()  Delete
 
-@app.get("/")
+# path + operation e.g. http://127.0.0.1:8000/mz/data/
+@app.get("/my/data/")
 async def root():
-    return {"message": "Hello FastAPI"}
+    return {"message": "Hello FastAPI"}  # it is possible to return dict, list, str, int, ect. 
