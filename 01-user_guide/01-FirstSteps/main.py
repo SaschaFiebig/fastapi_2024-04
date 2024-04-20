88c06e005308ae0cde2bcca02132266a8b340c5e
Sascha Fiebig <sascha.fiebig@posteo.net>
2024-04-20 16:14:30 +0200

    add more comments for FirstSteps main.py

--- 01-user_guide/01-FirstSteps/main.py
+++ 01-user_guide/01-FirstSteps/main.py
@@ -10,7 +10,7 @@ app = FastAPI()  # uvicorn main:app --reload (to start application)
 #@app.put()     Update
 #@app.delete()  Delete
 
-# path + operation e.g. http://127.0.0.1:8000/mz/data/
+# path operation decorator e.g. http://127.0.0.1:8000/mz/data/
 @app.get("/my/data/")
-async def root():
+async def root():  # path operation function
     return {"message": "Hello FastAPI"}  # it is possible to return dict, list, str, int, ect. 
